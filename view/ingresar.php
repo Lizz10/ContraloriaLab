cc4675f08de1247d344b08cc0648c163dd2d22a0
jose Gabriel <jguzmandev@gmail.com>
2016-11-01 11:37:34 -0500

    fr

--- view/ingresar.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$titulo="Ingresar";
-$style="css/ingresar.css";
-include_once 'plantilla/html-start.inc.php';
-include_once 'plantilla/login.inc.php';
-include_once 'plantilla/html-end.inc.php';
-?>
--- view/plantilla/html-end.inc.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-</body>
-</html>
